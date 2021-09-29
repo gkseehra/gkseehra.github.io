3b16768a43a7c3c6c7c418e45253b7a533d463bc
Gurleen <gurleenkseehra@gmail.com>
2021-09-29 23:38:41 +0530

    Added common code for nav-bar.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    $(function(){
+        $("#nav-bar").load("navBar.html");
+      });
+    // $("#nav-bar").load("navBar.html");
     var url = window.location.href; 
     $("nav a").each(function() {
         if(url == (this.href)) {
