95b88ee02baccb79548f6a83cda4e4fb51998703
Gurleen <gurleenkseehra@gmail.com>
2022-09-10 09:37:12 +0530

    Added content

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,30 @@
+console.log("Heyyyyyyyyyy!!")
 $(document).ready(function(){
-    $(function(){
-        $("#nav-bar").load("navBar.html");
-      });
-    // $("#nav-bar").load("navBar.html");
     var url = window.location.href; 
-    $("nav a").each(function() {
-        if(url == (this.href)) {
-            $(this).addClass("selected");
-        }
-    });
+
+        $("nav a").each(function() {
+            console.log(url)
+            console.log(this.href)
+            if(url == (this.href)) {
+                $(this).addClass("selected");
+            }
+        });
+
+
 });
+// TODO- add common nav-bar.
+// TODO- use list in nav-bar
+var myIndex = 0;
+startSlideShow();
+
+function startSlideShow() {
+  var i;
+  var x = document.getElementsByClassName("painting-slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
